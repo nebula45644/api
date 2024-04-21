277fbb9f6eadab0a8c6b54c95b5c299f505ea428
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2024-04-21 16:33:16 -0500

    Create aboutblank.js

---
+++ static/aboutblank.js
@@ -0,0 +1,48 @@
+let inFrame
+
+try {
+  inFrame = window !== top
+} catch (e) {
+  inFrame = true
+}
+
+if (!inFrame && !navigator.userAgent.includes('Firefox')) {
+  const popup = open('about:blank', '_blank')
+  if (!popup || popup.closed) {
+    alert('Click the Icon next to the star on the top right and click Always Allow. After then, press the Enter Key. This will hide the site from your history.')
+    location.reload();
+  } else {
+    const doc = popup.document
+    const iframe = doc.createElement('iframe')
+    const style = iframe.style
+    const link = doc.createElement('link')
+
+    const name = localStorage.getItem('name') || 'McDonald\'s'
+    const icon = localStorage.getItem('icon') || 'mcdonalds.ico'
+
+    doc.title = name
+    link.rel = 'icon'
+    link.href = icon
+
+    iframe.src = location.href
+    style.position = 'fixed'
+    style.top = style.bottom = style.left = style.right = 0
+    style.border = style.outline = 'none'
+    style.width = style.height = '100%'
+
+    doc.head.appendChild(link)
+    doc.body.appendChild(iframe)
+
+    const pLink = localStorage.getItem(encodeURI('pLink')) || '/execute.html'
+    location.replace(pLink)
+
+    const script = doc.createElement('script')
+    script.textContent = `
+      window.onbeforeunload = function (event) {
+        const confirmationMessage = 'Leave Site?';
+        (event || window.event).returnValue = confirmationMessage;
+        return confirmationMessage;
+      };
+    `
+  }
+}

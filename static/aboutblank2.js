4751b0b789f0b058d75a20ca699999b1681f9f64
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2024-04-21 16:33:34 -0500

    Create aboutblank2.js

---
+++ static/aboutblank2.js
@@ -0,0 +1,48 @@
+let inFrame
+
+try {
+  inFrame = window !== top
+} catch (e) {
+  inFrame = true
+}
+
+if (!inFrame && !navigator.userAgent.includes('Firefox')) {
+  const popup = open('about:blank', '_blank')
+  if (!popup || popup.closed) {
+    alert('Click the symbol next to the bookmark star and select "Always Allow Popups And Redirects" and hit save to hide the site from your history.')
+  } else {
+    const doc = popup.document
+    const iframe = doc.createElement('iframe')
+    const style = iframe.style
+    const link = doc.createElement('link')
+
+    const name = localStorage.getItem('name') || 'Home View Summary | Home Access Center'
+    const icon = localStorage.getItem('icon') || ''
+
+    doc.title = name
+    link.rel = 'icon'
+    link.href = icon
+
+    iframe.src = location.href
+    style.position = 'fixed'
+    style.top = style.bottom = style.left = style.right = 0
+    style.border = style.outline = 'none'
+    style.width = style.height = '100%'
+
+    doc.head.appendChild(link)
+    doc.body.appendChild(iframe)
+
+    const pLink = localStorage.getItem(encodeURI('pLink')) || 'https://accesscenter.roundrockisd.org/HomeAccess/Account/LogOn?ReturnUrl=%2fhomeaccess%2f'
+    location.replace(pLink)
+
+    const script = doc.createElement('script')
+    script.textContent = `
+      window.onbeforeunload = function (event) {
+        const confirmationMessage = 'Leave Site?';
+        (event || window.event).returnValue = confirmationMessage;
+        return confirmationMessage;
+      };
+    `
+    doc.head.appendChild(script)
+  }
+}

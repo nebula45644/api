13546dee7face5cb53f036294882311613b66d2a
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2024-04-23 11:32:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ const config = {
   users: {
     // You can add multiple users by doing username: 'password'.
     Jithesh: 'JigglyJithesh',
+    Ultimann: 'slavanish',
+    lukenigga: 'nigeria_',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.

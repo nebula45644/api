685b08f213e1af9fd3c773708d61282d51df5607
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2024-04-26 01:32:29 -0500

    Update js.js

--- static/js/js.js
+++ static/js/js.js
@@ -1,33 +1,3 @@
-/*
-  _____                   _                _     _                                                                      
- |  __ \                 | |              | |   | |                                                                     
- | |__) |   ___    _ __  | |_    ___    __| |   | |__    _   _                                                          
- |  ___/   / _ \  | '__| | __|  / _ \  / _` |   | '_ \  | | | |                                                         
- | |      | (_) | | |    | |_  |  __/ | (_| |   | |_) | | |_| |                                                         
- |_|       \___/  |_|     \__|  \___|  \__,_|   |_.__/   \__, |                                                         
-                                                          __/ |                                                         
-                                                         |___/                                                          
-                                _     _                     _       _   _          _                               _    
-     /\                        | |   | |                   | |     | \ | |        | |                             | |   
-    /  \     _ __ ___     ___  | |_  | |__    _   _   ___  | |_    |  \| |   ___  | |_  __      __   ___    _ __  | | __
-   / /\ \   | '_ ` _ \   / _ \ | __| | '_ \  | | | | / __| | __|   | . ` |  / _ \ | __| \ \ /\ / /  / _ \  | '__| | |/ /
-  / ____ \  | | | | | | |  __/ | |_  | | | | | |_| | \__ \ | |_    | |\  | |  __/ | |_   \ V  V /  | (_) | | |    |   < 
- /_/    \_\ |_| |_| |_|  \___|  \__| |_| |_|  \__, | |___/  \__|   |_| \_|  \___|  \__|   \_/\_/    \___/  |_|    |_|\_\
-                                               __/ |                                                                    
-                                              |___/                                                                     
-*/
-
-console.log(`Hypertabs
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.`)
 const prefix = __uv$config.prefix;
 const bare = new BareClient(__uv$config.bare);
 const URL_BAR = document.getElementById('urlbar');
@@ -61,7 +31,7 @@ const getActiveFrameId = () => {
 		);
 	return null;
 };
-document.title = 'Edficiency | Intervention Scheduling Software'; // New Title :)
+document.title = 'Home View Summary | Grades'; // New Title :)
 function addPageToHistory(id, page) {
 	if (!sir) {
 		sir = true;
@@ -288,7 +258,7 @@ let newTab = (url, uxor = true) => {
 	// creates a new hypertab!!
 	chromeTabs.addTab({
 		title: 'New Tab',
-                favicon: `https://www.google.com/s2/favicons?domain=${URL_BAR.value}`, 
+		favicon: '',
 	});
 
 	URL_BAR.value = '';
